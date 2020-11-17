467a750eeecdd9fc0f9bffef8df77b1363c3d068
ita7a <57022144+ita7a@users.noreply.github.com>
2020-11-17 07:10:37 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,11 @@
+try{$('.u-msg.dots').removeClass('dots');$(".ubnr").remove();$(`<link rel="stylesheet" href="https://rawcdn.githack.com/ita7a/MuHanadta7a/526583dcf9535c42b2e7c97d9b0298d6d613e1e6/ita7a.css"></style>`).insertBefore("body");$('.hand.nosel.fl.uzr.border').css({'margin':'1px 0',borderRight:'1px solid #EDEDED',borderLeft:'1px solid #adadad',borderTop:'1px solid #EDEDED',borderBottom:'1px solid #EDEDED',paddingTop:'3px',borderRadius:'5px'});$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" " id="ita7a" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> iTa7a.CoM | شات التحلية  </marquee></font></b></td>
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="Muhanad" name="FirstName" value="FM اذاعة التحلية ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://curiosity.shoutca.st:6035/ ؛"><a href="http://curiosity.shoutca.st:6035/ ؛</a></audio>
+
+<center></center></center>
+
+</div>`);
+
+$('#d0').append(`<label title="الاذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`);
